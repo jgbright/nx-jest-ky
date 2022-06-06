9f7d613f7883fe532afc885a7c3f0b146dadeecb
Jason Bright <jgbright@gmail.com>
2022-06-06 17:25:21 -0400

    update the component to use the `ky` library
    At this commit, the app correctly fetches data, but the tests will not compile.

--- libs/mylib/src/lib/Mylib.tsx
+++ libs/mylib/src/lib/Mylib.tsx
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+import ky from "ky";
+import React from 'react';
 
 /* eslint-disable-next-line */
 export interface MylibProps {}
@@ -7,10 +9,21 @@ const StyledMylib = styled.div`
   color: pink;
 `;
 
+type StarWarsPerson = {
+  name: string;
+};
+
 export function Mylib(props: MylibProps) {
+  const [state, setState] = React.useState<StarWarsPerson>();
+  React.useEffect(() => {
+    (async () => {
+      setState(await ky.get('https://swapi.dev/api/people/4').json());
+    })();
+  }, []);
   return (
     <StyledMylib>
       <h1>Welcome to Mylib!</h1>
+      <p>{state?.name ?? "Loading..."}</p>
     </StyledMylib>
   );
 }
